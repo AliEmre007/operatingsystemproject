bb6662df1ae2c8978be2b73ff8ca3309e3aad59c
GokberkEfe <167999829+GokberkEfe@users.noreply.github.com>
2024-05-23 22:03:29 +0300

    Add files via upload
    Scheduling Algorithms added and project finished.

--- osproject.c
+++ osproject.c
@@ -166,3 +166,159 @@ Process *sortByBurstTime(Process *queue, int count)
     }
     return sorted;
 }
+
+
+void firstComeFirstServe(Process* queue, int count) {            // Function to handle processes using the First-Come, First-Served (firstComeFirstServe) scheduling algorithm.
+    for (int i = 0; i < count; i++) {             // Iterate over each process in the queue.
+        char logAllert[MAX_ROW_LENGTH];        // Buffer for log messages.
+
+        if (!resourceCheck(queue[i].priority, queue[i].ram, queue[i].cpu)) {      // Check if there are sufficient resources.
+            snprintf(logAllert, sizeof(logAllert), 
+            "Process %s cannot be assigned to CPU-1 because of insufficient resources.", queue[i].id);  // Prepare a log message indicating insufficient resources.
+            fileLogging(logAllert);             // Log the message to the output file.
+            printf("%s\n", logAllert);          
+            continue;                             
+        }
+
+        snprintf(logAllert, sizeof(logAllert), "Process %s is queued to be assigned to CPU-1.", queue[i].id);  // Prepare a log message indicating the process is queued.
+        fileLogging(logAllert);        // Log the message to the output file.
+        printf("%s\n", logAllert);     
+
+        resourceAllocation(queue[i].priority, queue[i].ram, queue[i].cpu);  // Allocate resources for the current process.
+
+        snprintf(logAllert, sizeof(logAllert), "Process %s is assigned to CPU-1.", queue[i].id);  // Prepare a log message indicating the process is assigned to CPU-1.
+        fileLogging(logAllert);        // Log the message to the output file.
+        printf("%s\n", logAllert);     
+
+        snprintf(logAllert, sizeof(logAllert), "Process %s is completed and terminated.", queue[i].id);  // Prepare a log message indicating the process is completed and terminated.
+        fileLogging(logAllert);         // Log the message to the output file.
+        printf("%s\n", logAllert);      
+
+        resourceReleasing(queue[i].priority, queue[i].ram, queue[i].cpu);  // Release resources used by the current process.
+    }
+}
+
+
+void roundRobinScheduling(Process* queue, int count, int quantum) {
+    int* theRestOfBurstTime = (int*)malloc(count * sizeof(int));     // Allocate memory for an array to store remaining burst times for each process.
+    for (int i = 0; i < count; i++) {
+        theRestOfBurstTime[i] = queue[i].burst_time;                 // Initialize the remaining burst time for each process to its burst time.
+    }
+
+    int time = 0;        
+    while (1) {            // Continue looping indefinitely.
+        int done = 1;      // Flag to indicate if all processes are done.
+
+        for (int i = 0; i < count; i++) {           
+            if (theRestOfBurstTime[i] > 0) {      // Check if the process still has burst time remaining.
+                done = 0;              // Set the done flag to 0 indicating that there are still processes to be executed.
+
+                if (!resourceCheck(queue[i].priority, queue[i].ram, queue[i].cpu)) {     // Check if there are enough resources for the current process.
+                    char logAllert[MAX_ROW_LENGTH];     // Buffer for creating log messages.
+                    snprintf(logAllert, sizeof(logAllert), "Process %s cannot be assigned to CPU-2 because of insufficient resources.", queue[i].id);  // Create a log message 
+                    fileLogging(logAllert);          
+                    printf("%s\n", logAllert);       
+                    continue;                          
+                }
+
+                char logAllert[MAX_ROW_LENGTH];       // Buffer for creating log messages.
+
+                snprintf(logAllert, sizeof(logAllert), "Process %s is placed in the que2 queue to be assigned to CPU-2.", queue[i].id);  // Log message for placing the process in the queue.
+                fileLogging(logAllert);           
+                printf("%s\n", logAllert);        
+
+                resourceAllocation(queue[i].priority, queue[i].ram, queue[i].cpu);  // Allocate the necessary resources for the process.
+
+                snprintf(logAllert, sizeof(logAllert), "Process %s is assigned to CPU-2.", queue[i].id);  // Log message for assigning the process to CPU-2.
+                fileLogging(logAllert);              
+                printf("%s\n", logAllert);           
+
+                if (theRestOfBurstTime[i] > quantum) {       // Check if the remaining burst time is greater than the quantum.
+                    time += quantum;                           // Increment the time by the quantum.
+                    theRestOfBurstTime[i] -= quantum;        // Decrement the remaining burst time by the quantum.
+
+                    snprintf(logAllert, sizeof(logAllert), "Process %s ran for %d seconds and is queued again because the process is not completed.", queue[i].id, quantum);  // Log message for process running and queuing again.
+                    fileLogging(logAllert);             
+                    printf("%s\n", logAllert);          
+
+                    resourceReleasing(queue[i].priority, queue[i].ram, queue[i].cpu);  // Release the resources used by the process.
+                } else {
+                    time += theRestOfBurstTime[i];        // Increment the time by the remaining burst time.
+                    snprintf(logAllert, sizeof(logAllert), "Process %s ran for %d seconds, its operation is completed and terminated.", queue[i].id, theRestOfBurstTime[i]);  // Log message for process completion and termination.
+                    fileLogging(logAllert);               
+                    printf("%s\n", logAllert);            
+
+                    theRestOfBurstTime[i] = 0;            // Set the remaining burst time to 0 as the process is completed.
+
+                    resourceReleasing(queue[i].priority, queue[i].ram, queue[i].cpu);      // Release the resources used by the process.
+                }
+            }
+        }
+
+        if (done) break;       // If all processes are done, exit the loop.
+    }
+
+    free(theRestOfBurstTime);      // Free the memory allocated for the remaining burst time array.
+}
+
+
+void shortestJobFirst(Process* queue, int count) {
+    Process* sorted_queue = sortByBurstTime(queue, count);         // Sort the queue by burst time using the sortByBurstTime function.
+
+    for (int i = 0; i < count; i++) {               
+        char logAllert[MAX_ROW_LENGTH];           // Buffer for creating log messages.
+
+        if (!resourceCheck(sorted_queue[i].priority, sorted_queue[i].ram, sorted_queue[i].cpu)) {  // Check if there are enough resources for the current process.
+            snprintf(logAllert, sizeof(logAllert), "Process %s cannot be assigned to CPU-2 because of insufficient resources.", sorted_queue[i].id);  // Create a log message 
+            fileLogging(logAllert);              
+            printf("%s\n", logAllert);           
+            continue;         
+        }
+
+        snprintf(logAllert, sizeof(logAllert), "Process %s is placed in the que1 queue to be assigned to CPU-2.", sorted_queue[i].id);  // Log message for placing the process in the queue.
+        fileLogging(logAllert);             
+        printf("%s\n", logAllert);          
+
+        resourceAllocation(sorted_queue[i].priority, sorted_queue[i].ram, sorted_queue[i].cpu);  // Allocate the necessary resources for the process.
+
+        snprintf(logAllert, sizeof(logAllert), "Process %s is assigned to CPU-2.", sorted_queue[i].id);  // Log message for assigning the process to CPU-2.
+        fileLogging(logAllert);         
+        printf("%s\n", logAllert);      
+
+        snprintf(logAllert, sizeof(logAllert), "The operation of process %s is completed and terminated.", sorted_queue[i].id);  // Log message for process completion and termination.
+        fileLogging(logAllert);         
+        printf("%s\n", logAllert);      
+
+        resourceReleasing(sorted_queue[i].priority, sorted_queue[i].ram, sorted_queue[i].cpu);  // Release the resources used by the process.
+    }
+    free(sorted_queue);       
+}
+
+int main() {
+    read_processes("input.txt");           // Read the processes from the input file.
+
+    Process que1[MAX_PROCESSES], que2[MAX_PROCESSES], que3[MAX_PROCESSES], que4[MAX_PROCESSES];  // Create queues for each priority level.
+    int count1 = 0, count2 = 0, count3 = 0, count4 = 0;       // Initialize counts for each queue.
+
+    for (int i = 0; i < process_tracker; i++) {        // Iterate over all processes.
+        switch (processes[i].priority) {             // Switch based on the priority of each process.
+            case 0: que1[count1++] = processes[i]; break;      
+            case 1: que2[count2++] = processes[i]; break;      
+            case 2: que3[count3++] = processes[i]; break;      
+            case 3: que4[count4++] = processes[i]; break;      
+        }
+    }
+
+    printf("************* OUTPUT OF THE PROGRAM *************\n"); 
+
+    firstComeFirstServe(que1, count1);                // Execute firstComeFirstServe scheduling for que1.
+    shortestJobFirst(que2, count2);                 // Execute shortestJobFirst scheduling for que2.
+    roundRobinScheduling(que3, count3, 8);      // Execute round robin scheduling for que3 with quantum 8.
+    roundRobinScheduling(que4, count4, 16);     // Execute round robin scheduling for que4 with quantum 16.
+
+    printf("**********************\n");  
+    log_queues();         
+    printf("**********************\n");  
+
+    return 0;         
+}
