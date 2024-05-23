51226ccf2a4926fe66a3a8117cfc45519ed7f70d
sahankurt <67442656+sahankurt@users.noreply.github.com>
2024-05-23 21:42:39 +0300

    Add files via upload
    File logging and Read Process functions are added.

--- osproject.c
+++ osproject.c
@@ -25,3 +25,35 @@ int process_tracker = 0;          // Counter to keep track of the number of proc
 int available_ram_priority_0 = RESERVED_RAM_PRIORITY_0;
 int available_ram_other = TOTAL_RAM - RESERVED_RAM_PRIORITY_0;
 int available_cpu = CPU_LIMIT;
+
+void read_processes(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
+        perror("Could not open input file"); // Print error message if the file could not be opened.
+        exit(EXIT_FAILURE);                  // Exit the program with failure status.
+    }
+
+    char line[MAX_ROW_LENGTH]; // Buffer to store each line read from the file.
+    while (fgets(line, sizeof(line), file))
+    {                                                                                                            // Read each line from the file.
+        Process p;                                                                                               // Create a temporary Process variable to hold the data.
+        sscanf(line, "%[^,],%d,%d,%d,%d,%d", p.id, &p.arrival_time, &p.priority, &p.burst_time, &p.ram, &p.cpu); // Parse the line and store the values in the Process variable.
+        processes[process_tracker++] = p;                                                                        // Add the Process to the array and increment the process count.
+    }
+
+    fclose(file); // Close the input file.
+}
+
+void fileLogging(const char *message)
+{                                          // Function to log a message to a file.
+    FILE *file = fopen("output.txt", "a"); // Open the output file in append mode.
+    if (!file)
+    {                                            // Check if the file was opened successfully.
+        perror("opening error for output file"); // Print error message if the file could not be opened.
+        exit(EXIT_FAILURE);
+    }
+    fprintf(file, "%s\n", message); // Write the message to the file, followed by a newline.
+    fclose(file);                   // Close the output file.
+}
